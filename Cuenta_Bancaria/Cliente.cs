db2deece9b69ce20cc996352daa5a11d9587e5c9
Mauricioagreda <mauricio.agreda.d2@gmail.com>
2021-05-19 16:02:16 -0300

    primero

---
+++ Cuenta_Bancaria/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cuenta_Bancaria
+{
+    public class Cliente
+    {
+        private Cuenta Cuenta;
+        private string Nombre { get; set; }
+        private string Apellido { get; set; }
+        private double Efectivo { get; set; }
+
+        public double Saldo => this.Efectivo + Cuenta.Saldo;
+
+        public Cliente()
+        {
+            Efectivo = 0;
+            Cuenta = new Cuenta();
+        }
+
+        public void Acreditar(double acreditar)
+        {
+            Efectivo = Efectivo + acreditar * 0.8;
+            Cuenta.Acreditar(acreditar * 0.2);
+        }
+
+        public void Debitar(double debitar)
+        {
+            Efectivo = Efectivo - debitar * 0.8;
+            Cuenta.Debitar(debitar * 0.2);
+        }
+
+        public int Cbu => Cuenta.Cbu;
+
+        public bool TieneAlMenos(double x) => Saldo >= x;
+    }
+}
---
+++ Cuenta_Bancaria/Cuenta.cs
@@ -0,0 +1,27 @@
+﻿namespace Cuenta_Bancaria
+{
+    public class Cuenta
+    {
+        static int Contador { get; set; } = 0;
+        public int Cbu { get; set; }
+        public double Saldo { get; set; }
+
+        public void Acreditar(double acreditar)
+        {
+            Saldo += acreditar;
+        }
+
+        public void Debitar(double debitar)
+        {
+            Saldo -= debitar;
+        }
+
+        public bool TieneAlMenos(double x) => Saldo >= x;
+
+        public Cuenta()
+        {
+            Cbu = ++Contador;
+            Saldo = 0;
+        }
+    }
+}
